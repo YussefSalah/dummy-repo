58dc230c70e134461a8d584ba548d415db8f81a3
youssef2272002salah <youssef22772002salah@gmail.com>
2026-08-14 16:32:40 +0300

    Add clean backend codebase only

--- backend/dist/orders/dto/checkout.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CheckoutDto {
-    user_name: string;
-    shippingAddress: string;
-    city: string;
-    postalCode: string;
-    country: string;
-    paymentMethod: string;
-}
