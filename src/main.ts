931d8daa99c739642e20213263626a075850712c
youssef2272002salah <youssef22772002salah@gmail.com>
2026-08-16 20:07:19 +0300

    style: adjust whitespace formatting in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, BadRequestException, Logger } from '@nestjs/common';
@@ -5,7 +7,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,6 +16,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
 
 
   const config = new DocumentBuilder()
